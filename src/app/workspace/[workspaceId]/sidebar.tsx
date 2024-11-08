801e4d828bedcab40491a6c63fcb39bb9b848693
belief2002 <satyampatel30smart@gmail.com>
2024-11-08 15:33:52 +0530

    sidebar component

--- src/app/workspace/[workspaceId]/sidebar.tsx
+++ src/app/workspace/[workspaceId]/sidebar.tsx
@@ -1,15 +1,22 @@
-import { UserButton } from "@/features/auth/components/user-button"
-import { WorkspaceSwitcher } from "./workspace-switcher"
+import { UserButton } from "@/features/auth/components/user-button";
+import { WorkspaceSwitcher } from "./workspace-switcher";
+import { SideBarButton } from "./sidebar-button";
+import { Bell, Home, MessageSquare, MoreHorizontal } from "lucide-react";
+import { usePathname } from "next/navigation";
 
-export const Sidebar = ()=>{
-    return (
-        <aside className="w-[70px] h-full flex flex-col gap-y-4 items-center pt-[9xp] pb-4 bg-[#481349]">
-            <div>
-                <WorkspaceSwitcher/>
-            </div>
-            <div className="flex flex-col items-center justify-center gap-y-1 mt-auto">
-                <UserButton/>
-            </div>
-        </aside>
-    )
-}
+export const Sidebar = () => {
+    const pathname = usePathname()
+  return (
+    <aside className="w-[70px] h-full flex flex-col gap-y-4 items-center pt-[9xp] pb-4 bg-[#481349]">
+      <WorkspaceSwitcher />
+      <SideBarButton icon={Home} label="Home" isActive={pathname.includes("/workspace")}/>
+      <SideBarButton icon={MessageSquare} label="DMs" />
+      <SideBarButton icon={Bell} label="Activity" />
+      <SideBarButton icon={MoreHorizontal} label="More" />
+
+      <div className="flex flex-col items-center justify-center gap-y-1 mt-auto">
+        <UserButton />
+      </div>
+    </aside>
+  );
+};
