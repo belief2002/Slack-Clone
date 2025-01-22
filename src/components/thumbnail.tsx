b4a9632cb04b22ed0f2b4f902e4f8230f95443c7
belief2002 <satyampatel30smart@gmail.com>
2025-01-22 20:18:07 +0530

    added scroll to image dialog in message

--- src/components/thumbnail.tsx
+++ src/components/thumbnail.tsx
@@ -5,6 +5,7 @@ import {
   DialogTrigger,
   DialogTitle,
 } from "./ui/dialog";
+import { ScrollArea } from "./ui/scroll-area";
 interface ThumbnailProps {
   url: string | undefined | null;
 }
@@ -13,22 +14,25 @@ export const Thumbnail = ({ url }: ThumbnailProps) => {
   if (!url) return null;
 
   return (
+    
     <Dialog>
       <DialogTrigger>
         <div className="relative overflow-hidden max-w-[360px]  rounded-lg border my-2 cursor-zoom-in">
           <img
             src={url}
             alt="image"
-            className="rounded-md size-full object-contain"
+            className="rounded-md size-full object-cover"
           />
         </div>
       </DialogTrigger>
-      <DialogContent className="maz-w-[800px] border-none bg-transparent p-0 shadow-none">
+      <DialogContent className="maz-w-[800px]  border-none h-auto bg-transparent p-0 shadow-none">
+      <ScrollArea className="max-h-[700px]">
         <img
           src={url}
           alt="image"
-          className="rounded-md  size-full object-cover"
+          className="rounded-md h-auto  size-full object-cover"
         />
+    </ScrollArea>
       </DialogContent>
     </Dialog>
   );
